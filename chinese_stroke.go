94a66488c6affc87e81dc470728dd06a8fe9228b
Fayland Lam <fayland@gmail.com>
2013-12-25 23:01:52 +0800

    fix path

--- chinese_stroke.go
+++ chinese_stroke.go
@@ -21,7 +21,7 @@ func init() {
 
 	// FIXME
 	gopath := os.Getenv("GOPATH")
-	filepath := path.Join(gopath, "/chinese_stroke/dat/Stroke.dat")
+	filepath := path.Join(gopath, "/src/github.com/fayland/chinese_stroke/dat/Stroke.dat")
 
 	file, err := os.Open(filepath)
 	if err != nil {
